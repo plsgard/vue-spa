39ff6c31446b09d082b5b735279008173e704629
Pier-Lionel Sgard <hello@plsgd.com>
2018-03-14 15:42:51 +0100

    Finalize Routing module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import AppLayout from './theme/Layout.vue'
+import router from './router'
 
 const app = new Vue({
-  ...AppLayout
+  ...AppLayout,
+  router: router
 })
 
-export { app }
+export { app, router }
